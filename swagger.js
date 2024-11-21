1e6771eda9de1081d9dbcfbed27c9498b89d1e0b
joy935 <tupaiv@gmail.com>
2024-11-21 15:52:34 +0100

    updated to render host

--- swagger.js
+++ swagger.js
@@ -1,17 +1,17 @@
-const { Schema } = require('mongoose');
+const { Schema } = require("mongoose");
 
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
   info: {
-    title: 'Contacts API',
-    description: 'Simple API to manage contacts',
+    title: "Contacts API",
+    description: "Simple API to manage contacts",
   },
-  host: 'localhost:3030',
-  schemes: ['http']
+  host: "https://cse341-project2-mszp.onrender.com",
+  schemes: ["https", "http"]
 };
 
-const outputFile = './swagger.json';
-const routes = ['./routes/index.js'];
+const outputFile = "./swagger.json";
+const routes = ["./routes/index.js"];
 
 swaggerAutogen(outputFile, routes, doc);
