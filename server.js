a13054f338419aab37085ef5b6282784a0d6ffa1
joy935 <tupaiv@gmail.com>
2024-11-21 13:59:49 +0100

    added api doc

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const mongodb = require("./data/database");
 
 const PORT = process.env.PORT || 3030;
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 const routes = require('./routes');
 app.use('/', routes);
---
+++ swagger.js
@@ -0,0 +1,17 @@
+const { Schema } = require('mongoose');
+
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description: 'Simple API to manage contacts',
+  },
+  host: 'localhost:3030',
+  schemes: ['http']
+};
+
+const outputFile = './swagger.json';
+const routes = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, routes, doc);
