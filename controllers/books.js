99e833ecb22e51fa3b617c8e260be34e0514f6cb
joy935 <tupaiv@gmail.com>
2024-11-22 09:48:49 +0100

    added swagger tags

--- controllers/books.js
+++ controllers/books.js
@@ -2,6 +2,7 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAllBooks = async (req, res) => {
+    // #swagger.tags = ['Books']
     try {
         const result = await mongodb.getDb().db().collection("books").find();
         result.toArray().then((books) => {
@@ -14,9 +15,13 @@ const getAllBooks = async (req, res) => {
 };
 
 const getOneBook = async (req, res) => {
+    // #swagger.tags = ['Books']
     try {
         const bookId = new ObjectId(req.params.id);
         const result = await mongodb.getDb().db().collection("books").find({ _id: bookId });
+        if (!result) {
+            res.status(404).json("Book not found.");
+        }
         result.toArray().then((books) => {
             res.setHeader("Content-Type", "application/json");
             res.status(200).json(books[0]);
@@ -27,55 +32,70 @@ const getOneBook = async (req, res) => {
 };
 
 const addBook = async (req, res) => {
-    const book = {
-        title: req.body.title,
-        subtitle: req.body.subtitle,
-        isbn: req.body.isbn,
-        author: req.body.author,
-        publisher: req.body.publisher,
-        publishedDate: req.body.publishedDate,
-        genre: req.body.genre
-    };
-    const response = await mongodb.getDb().db().collection("books").insertOne(book);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json("Some error occurred while adding the book.");
+    // #swagger.tags = ['Books']
+    try {
+        const book = {
+            title: req.body.title,
+            subtitle: req.body.subtitle,
+            isbn: req.body.isbn,
+            author: req.body.author,
+            publisher: req.body.publisher,
+            publishedDate: req.body.publishedDate,
+            genre: req.body.genre
+        };
+        const response = await mongodb.getDb().db().collection("books").insertOne(book);
+        if (response.acknowledged) {
+            res.status(204).send();
+        } else {
+            res.status(500).json("Some error occurred while adding the book.");
+        }
+    } catch (error) {
+        res.status(500).json(error || "An unexpected error occurred.");
     }
 };
 
 const updateBook = async (req, res) => {
-    const bookId = new ObjectId(req.params.id);
-    if (!bookId) {
-        res.status(500).json("Book ID not found.");
-    }
-    const book = {
-        title: req.body.title,
-        subtitle: req.body.subtitle,
-        isbn: req.body.isbn,
-        author: req.body.author,
-        publisher: req.body.publisher,
-        publishedDate: req.body.publishedDate,
-        genre: req.body.genre
-    };
-    const response = await mongodb.getDb().db().collection("books").replaceOne({ _id: bookId }, book);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json("Some error occurred while updating the book.");
+    // #swagger.tags = ['Books']
+    try {
+        const bookId = new ObjectId(req.params.id);
+        if (!bookId) {
+            res.status(500).json("Book ID not found.");
+        }
+        const book = {
+            title: req.body.title,
+            subtitle: req.body.subtitle,
+            isbn: req.body.isbn,
+            author: req.body.author,
+            publisher: req.body.publisher,
+            publishedDate: req.body.publishedDate,
+            genre: req.body.genre
+        };
+        const response = await mongodb.getDb().db().collection("books").replaceOne({ _id: bookId }, book);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json("Some error occurred while updating the book.");
+        }
+    } catch (error) {
+        res.status(500).json(error || "An unexpected error occurred.");
     }
 };
 
 const deleteBook = async (req, res) => {
-    const bookId = new ObjectId(req.params.id);
-    if (!bookId) {
-        res.status(500).json("Book ID not found.");
-    }
-    const response = await mongodb.getDb().db().collection("books").deleteOne({ _id: bookId }, true);
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json("Some error occurred while deleting the book.");
+    // #swagger.tags = ['Books']
+    try {
+        const bookId = new ObjectId(req.params.id);
+        if (!bookId) {
+            res.status(500).json("Book ID not found.");
+        }
+        const response = await mongodb.getDb().db().collection("books").deleteOne({ _id: bookId }, true);
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json("Some error occurred while deleting the book.");
+        }
+    } catch (error) {
+        res.status(500).json(error || "An unexpected error occurred.");
     }
 };
 
