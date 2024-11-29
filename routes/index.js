5a49acf3e18240acc60b5a49d70d0e122a884c94
joy935 <tupaiv@gmail.com>
2024-11-29 12:07:00 +0100

    removed path

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,7 @@
 const passport = require("passport");
-
 const router = require("express").Router();
 
 router.use("/", require("./swagger"));
-
-router.get("/", (req, res) => {
-    // #swagger.tags = ['Hello World']
-    res.send("Hello World");
-    });
-
 router.use("/books", require("./books"));
 router.use("/users", require("./users"));
 
